b1fa4d4994dadb8cd8ea22cd0109fea638a945e6
Tan Tran <vinhtantran@gmail.com>
2020-10-12 00:10:52 -0400

    done predict

--- R/cv.test.R
+++ R/cv.test.R
@@ -1,56 +1,96 @@
-#' k-fold Cross validation test on a monothetic clustering result.
+#' Cross-Validation Test on MonoClust
 #'
-#' @param data data set
-#' @param fold number of folds (k)
-#' @param minnodes the minimum number of clusters to be checked
-#' @param maxnodes the maximum number of clusters to be checked
-#' @param ...
+#' Perform cross-validation test for different different number of clusters of
+#' Monothetic Clustering.
+#'
+#' @param data Data set to be partitioned.
+#' @param fold Number of folds (k). When `k = 1`, the function performs a
+#' Leave-One-Out-Cross-Validation (LOOCV). When `k > 1`, a data will be
+#' returned.
+#' @param minnodes Minimum number of clusters to be checked.
+#' @param maxnodes Maximum number of clusters to be checked.
+#' @param ... Other parameters transferred to [MonoClust()].
+#'
+#' @details
+#' The \eqn{k}-fold cross-validation randomly partitions data into \eqn{k} subsets with
+#' equal (or close to equal) sizes. \eqn{k - 1} subsets are used as the training
+#' data set to create a tree with a desired number of leaves and the other
+#' subset is used as validation data set to evaluate the predictive performance
+#' of the trained tree. The process repeats for each subset as the validating
+#' set (\eqn{m = 1, \ldots, k}) and the mean squared difference,
+#' \deqn{MSE_m=\frac{1}{n_m} \sum_{q=1}^Q\sum_{i \in m} d^2_{euc}(y_{iq}, \hat{y}_{(-i)q}),}
+#' is calculated, where \eqn{\hat{y}_{(-i)q}} is the cluster mean on the variable
+#' \eqn{q} of the cluster created by the training data where the observed value,
+#' \eqn{y_{iq}}, of the validation data set will fall into, and
+#' \eqn{d^2_{euc}(y_{iq}, \hat{y}_{(-i)q})} is the squared Euclidean distance
+#' (dissimilarity) between two observations at variable $q$. This process is
+#' repeated for the $k$ subsets of the data set and the average of these test
+#' errors is the cross-validation-based estimate of the mean squared error of
+#' predicting a new observation,
+#' \deqn{CV_K = \overline{MSE} = \frac{1}{M} \sum_{m=1}^M MSE_m.}
 #'
 #' @return A list of sum of squares of difference between the predicted and true
-#' values
+#'   values.
+#' @seealso [cv.plot()], [MonoClust()], [predict.MonoClust()]
 #' @export
 #'
 #' @importFrom stats sd
 #'
-#' @examples NULL
+#' @examples
+#' library(cluster)
+#' data(ruspini)
+#' cv.test(ruspini, minnodes = 2, maxnodes = 4)
 cv.test <- function(data, fold = 10, minnodes = 2, maxnodes = 10, ...) {
 
-    SSET <- numeric(0)
-    num.obs <- dim(data)[1]
-    # LOOCV
-    if (fold == 1) {
-        for (k in minnodes:maxnodes) {
-            SSEi <- numeric(0)
-            fullc <- MonoClust(data, nclusters = k)
-            # pRsq=c(pRsq,adonis(dist(data)~factor(fullc$Membership),permutations=1)$aov.tab[1,5])
-            for (i in seq(length(data[, 1]))) {
-                out <- MonoClust(data[-i, ], nclusters = k)
-                predict.MonoClust(out, newdata = data[i, ])
-                SSEi <- c(SSEi, sum((data[i, ] - predict.MonoClust(out, newdata = data[i, ])[, -1])^2))
-            }
+  if (!is.data.frame(data)) {
+    stop("\"data\" must be a data frame.")
+  }
 
-            SSET <- c(SSET, sum(SSEi))
+  num_obs <- nrow(data)
+  # LOOCV
+  if (fold == 1) {
+    # sse_t is a vector for LOOCV
+    sse_t <- vector("double", maxnodes - minnodes + 1)
+    for (k in minnodes:maxnodes) {
+      sse_i <- vector("double", num_obs)
+      # fullc <- MonoClust(data, nclusters = k)
+      for (i in seq_len(num_obs)) {
+        out <- MonoClust(data[-i, ], nclusters = k, ...)
+        predict.MonoClust(out, newdata = data[i, ])
+        sse_i[i] <-
+          sum((data[i, ] -
+                 predict.MonoClust(out,
+                                   newdata = data[i, ])[, -1])^2)
+      }
 
-        }
-    } else if (fold > 1) {
-        index <- rep(1:fold, num.obs%/%fold + 1)
-        random.list <- sample(index, num.obs, replace = FALSE)
+      sse_t[k] <- sum(sse_i)
 
-        for (k in minnodes:maxnodes) {
-            SSEi <- numeric(0)
-            for (i in 1:fold) {
-                train.set <- data[-which(random.list == i), ]
-                test.set <- data[which(random.list == i), ]
-                train.tree <- MonoClust(train.set, nclusters = k)
-                SSEi <- c(SSEi, sum((test.set - predict.MonoClust(train.tree, test.set)[, -1])^2))
-            }
-            SSET <- rbind(SSET, c(mean(SSEi), sd(SSEi)))
-
-        }
-        colnames(SSET) <- c("MSE", "Std. Dev.")
-        rownames(SSET) <- seq(minnodes, maxnodes, 1)
     }
+  } else if (fold > 1) {
+    # sse_t is a list of vector, later will be bound into a tibble
+    sse_t <- vector("list", maxnodes - minnodes + 1)
+    index <- rep(1:fold, num_obs %/% fold + 1)
+    random_list <- sample(index, num_obs, replace = FALSE)
 
-    SSET
+    for (k in minnodes:maxnodes) {
+      sse_i <- vector("double", fold)
+      for (i in 1:fold) {
+        train_set <- data[-which(random_list == i), ]
+        test_set <- data[which(random_list == i), ]
+        train_tree <- MonoClust(train_set, nclusters = k)
+        sse_i[i] <-
+          sum((test_set -
+                 predict.MonoClust(train_tree,
+                                   test_set)[, -1])^2)
+      }
+      sse_t[k] <- c(mean(sse_i), sd(sse_i))
+
+    }
+    sse_t <- purrr::flatten_dfr(sse_t)
+    colnames(sse_t) <- c("MSE", "Std. Dev.")
+    rownames(sse_t) <- seq(minnodes, maxnodes, 1)
+  }
+
+  sse_t
 
 }
--- R/predict.MonoClust.R
+++ R/predict.MonoClust.R
@@ -1,102 +1,121 @@
-#' Title
+#' Predictions from a MonoClust Object
 #'
-#' @param object
-#' @param newdata
-#' @param na.action
-#' @param type
-#' @param ...
+#' Predict the cluster memberships of a new data set from a `MonoClust` object.
 #'
-#' @return
+#' @param object MonoClust result object.
+#' @param newdata Data frame containing the values to be predicted. If missing,
+#'   the memberships of the MonoClust object are returned.
+#' @param type Type of returned cluster representatives. Either `"centroid"` to
+#'   return the centroid values of the terminal clusters, or `"medoid"` to
+#'   return the index of the medoid observations in the clustered data set.
+#'
+#' @return A tibble of cluster index in `cname` and either centroid values or
+#'   medoid observations index based on the value of `type` argument.
+#'
+#' @seealso [predict]
 #' @export
 #'
 #' @importFrom stats na.pass
 #'
 #' @examples
-predict.MonoClust <- function(object, newdata = list(), na.action = na.pass, type = "mean", ...) {
+#' library(cluster)
+#' data(ruspini)
+#'
+#' set.seed(1234)
+#' test_index <- sample(1:nrow(ruspini), nrow(ruspini)/5)
+#' train_index <- setdiff(1:nrow(ruspini), test_index)
+#' ruspini_train <- ruspini[train_index, ]
+#' ruspini_test <- ruspini[test_index, ]
+#'
+#' ruspini_train_4sol <- MonoClust(ruspini_train, nclusters = 4)
+#' predict(ruspini_train_4sol, newdata = ruspini_test)
+predict.MonoClust <- function(object, newdata, type = c("centroid", "medoid")) {
 
-    ####### DELETE: For debugging purpose ########### require(cluster) data(ruspini) source('MonoClust.R')
-    ####### object <- MonoClust(ruspini, nclusters = 4) newdata <- list(x = c(48, 90), y = c(93, 70))
-    ####### na.action <- na.pass
+  ####### DELETE: For debugging purpose ########### require(cluster) data(ruspini) source('MonoClust.R')
+  ####### object <- MonoClust(ruspini, nclusters = 4) newdata <- list(x = c(48, 90), y = c(93, 70))
+  ####### na.action <- na.pass
 
-    if (!inherits(object, "rpart"))
-        stop("Not a legitimate \"rpart\" object")
+  if (!inherits(object, "MonoClust"))
+    stop("Not a legitimate \"MonoClust\" object")
 
-    where <- if (missing(newdata))
-        object$Membership else {
-        frame <- object$frame
-        new.data <- data.frame(newdata)
-        # Check to see if the newdata has the same structure as the object
-        Terms <- object$terms
-        new.terms <- colnames(new.data)
-        if (!length(intersect(Terms, new.terms)) == length(Terms))
-            stop("The new data set does not have the same variables as the original data set")
+  type <- match.arg(type)
 
-        node <- as.numeric(row.names(frame))
-        depth <- tree_depth(node)
+  terminal_node <- NA
+  if (missing(newdata)) {
+    terminal_node <- object$Membership
+  } else {
+    frame <- object$frame
+    new_data <- tibble::tibble(newdata)
+    # Check to see if the newdata has the same structure as the object
+    terms <- object$terms
+    new_terms <- colnames(new_data)
+    if (!length(intersect(terms, new_terms)) == length(terms))
+      stop("The new data set does not have the same variables as the original
+           data set")
 
-        # It would be better to create a jump table for reference of tree walking
-        jump.table <- cbind(node, frame[, c("bipartvar", "cut")])
-        jump.table$right <- jump.table$left <- NA
+    # node <- frame$number
+    # depth <- tree_depth(node)
 
-        if (nrow(jump.table) >= 2) {
-            for (i in 2:nrow(jump.table)) {
-                parent.node <- jump.table$node[i]%/%2
-                is.left <- jump.table$node[i]%%2 == 0
+    # It would be better to create a jump table for reference of tree walking
+    jump_table <- dplyr::select(frame, number, bipartvar, cut)
+    jump_table <- tibble::add_column(jump_table, left = NA, right = NA)
 
-                if (is.left) {
-                  jump.table$left[which(jump.table$node == parent.node)] <- i
-                } else jump.table$right[which(jump.table$node == parent.node)] <- i
-            }
-        } else jump.table$bipartvar <- jump.table$cut <- NA
+    if (nrow(jump_table) >= 2) {
+      jump_table$left <- match(jump_table$number * 2, jump_table$number)
+      jump_table$right <- match(jump_table$number * 2 + 1, jump_table$number)
+    } else jump_table[, c("bipartvar", "number")] <- NA
 
-        # Now tracing the tree to find the cluster
-        apply(new.data, 1, tree.walk, jump.table = jump.table)
-    }
+    # Now tracing the tree to find the cluster
+    terminal_node <- apply(new_data, 1, tree_walk, jump_table = jump_table)
+  }
+  # ylevels <- attr(object, "ylevels")
+  # nclass <- length(ylevels)
+  # if (missing(type) && nclass > 0L)
+  #   type <- "prob"
+  if (type == "centroid") {
+    centroids <- object$centroids
+    ret <- centroids[match(terminal_node, centroids$cname), ]
+  } else {
+    ret <- tibble::tibble(cname = terminal_node,
+                  medoid = object$frame$medoid[terminal_node])
+  }
+  # } else if (type == 'matrix') { pred <- frame$yval2[terminal_node, ] dimnames(pred) <-
+  # list(names(terminal_node), NULL) } else if (type == 'class' && nclass > 0L) { if (length(ylevels) ==
+  # 0L) stop('type 'class' is only appropriate for classification') pred <-
+  # factor(ylevels[frame$yval[terminal_node]], levels = ylevels) names(pred) <- names(terminal_node) } else if
+  # (type == 'prob' && nclass > 0L) { pred <- frame$yval2[terminal_node, 1L + nclass + 1L:nclass, drop =
+  # FALSE] dimnames(pred) <- list(names(terminal_node), ylevels) } else stop('Invalid prediction for
+  # \'rpart\' object')
 
-
-
-    mtype <- missing(type)
-    type <- match.arg(type)
-    ylevels <- attr(object, "ylevels")
-    nclass <- length(ylevels)
-    if (mtype && nclass > 0L)
-        type <- "prob"
-    if (type == "mean" && is.null(frame$yval2)) {
-        centroids <- data.frame(object$centroids)
-        ret <- centroids[sapply(where, function(x) which(x == centroids[, 1])), ]
-    }
-    # } else if (type == 'matrix') { pred <- frame$yval2[where, ] dimnames(pred) <-
-    # list(names(where), NULL) } else if (type == 'class' && nclass > 0L) { if (length(ylevels) ==
-    # 0L) stop('type 'class' is only appropriate for classification') pred <-
-    # factor(ylevels[frame$yval[where]], levels = ylevels) names(pred) <- names(where) } else if
-    # (type == 'prob' && nclass > 0L) { pred <- frame$yval2[where, 1L + nclass + 1L:nclass, drop =
-    # FALSE] dimnames(pred) <- list(names(where), ylevels) } else stop('Invalid prediction for
-    # \'rpart\' object')
-
-    # Expand out the missing values in the result But only if operating on the original dataset if
-    # (missing(newdata) && !is.null(object$na.action)) pred <- naresid(object$na.action, pred)
-    ret
-
+  # Expand out the missing values in the result But only if operating on the original dataset if
+  # (missing(newdata) && !is.null(object$na.action)) pred <- naresid(object$na.action, pred)
+  return(ret)
 }
 
-tree.walk <- function(jump.table, new.point) {
-    current.node <- 1
-    repeat {
-        # Check stopping at leaf node
-        if (is.na(jump.table$bipartvar[current.node]))
-            break
+#' Traverse a Tree to Find the Leaves (Terminal Nodes)
+#'
+#' @param new_point New data point
+#' @param jump_table Jump table
+#'
+#' @return The index of the terminal node after traversing the new data point on
+#'   the tree.
+#'
+#' @keywords internal
+tree_walk <- function(new_point, jump_table) {
+  current_node <- 1
+  while (!is.na(jump_table$cut[current_node])) {
+    # If it's not a leaf node, trace
+    var <- jump_table$bipartvar[current_node]
+    value <- new_point[var]
 
-        # If it's not a leaf node, trace
-        var <- jump.table$bipartvar[current.node]
-        value <- new.point[var]
+    current_node <- dplyr::if_else(value < jump_table$cut[current_node],
+                                   jump_table$left[current_node],
+                                   jump_table$right[current_node])
+  }
 
-        current.node <- as.numeric(ifelse(value < jump.table$cut[current.node], jump.table$left[current.node],
-            jump.table$right[current.node]))
-    }
-
-    # Extract the leaf info out. TODO: Output the necessary info.  QUESTION: Do we need to
-    # re-calculate any properties? Like medoid?
-    node <- as.numeric(row.names(jump.table)[current.node])
-    names(node) <- current.node
-    node
+  # Extract the leaf info out. TODO: Output the necessary info.
+  # QUESTION: Do we need to re-calculate any properties? Like medoid?
+  node <- jump_table$number[current_node]
+  names(node) <- current_node
+  return(node)
 }
