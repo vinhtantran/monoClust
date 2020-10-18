c2afff4bffb2042e8a0eedae1ec32e9d5c31d73e
Tan Tran <vinhtantran@gmail.com>
2020-10-17 23:36:36 -0400

    depends on R > 3.0.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
 ## This file is a hack to remove false flags in R CMD check when using foreach
 ## iterations.
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c("iter", "k"))
-}
+utils::globalVariables(c("iter", "k"))
